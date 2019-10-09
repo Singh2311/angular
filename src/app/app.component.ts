eb81df830e7e13c3f0fef6c18e7dad389e4bae8f
Singh2311 <ss9532729@gmail.com>
2019-10-09 20:44:57 +0530

    lets begin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-hello-world';
+addArticle(title:HTMLInputElement,link:HTMLInputElement){
+  console.log(`article is ${title.value} and link ${link.value}`);
+}
 }
